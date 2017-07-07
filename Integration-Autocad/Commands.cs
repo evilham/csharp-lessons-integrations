ea6beaddf28436adeca0879919cb34a5d2f5e370
Evilham <github@evilham.com>
2017-07-07 03:27:25 +0200

    Added Integration-{Autocad,CoreLib}.
    Integration-Autocad is a simple Autocad plugin that dumps polylines as json.
    Integration-CoreLib is a simple library to share classes between Excel and
    Autocad Integrations.

---
+++ Integration-Autocad/Commands.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Autodesk.AutoCAD.ApplicationServices.Core;
+using Autodesk.AutoCAD.Runtime;
+using Autodesk.AutoCAD.DatabaseServices;
+
+using Newtonsoft.Json;
+
+[assembly: CommandClass(typeof(PolygonData.Commands))]
+namespace PolygonData
+{
+    public sealed class Commands
+    {
+        /// <summary>
+        /// Private constructor to keep this class from being instantiated.
+        /// This class is sealed as well, since we don't intend other
+        /// classes to inherit from it
+        /// </summary>
+        private Commands() {}
+
+        /// <summary>
+        /// Call from inside AutoCAD with PolyData.
+        /// Generates a text file with JSON objects representing the polygons.
+        /// </summary>
+        [CommandMethod("polydata", "polydata", "polydata",
+         CommandFlags.Modal & CommandFlags.Session)]
+        public static void PolyDataCommand()
+        {
+            // Get Autocad document
+            var acDoc = Application.DocumentManager.CurrentDocument;
+            // Any direct access to the objects must be wrapped in a transaction
+            // A DWG file is a specialised database.
+            using (var acTrans = acDoc.Database.TransactionManager.StartTransaction())
+            {
+                // Get model space object for read. For this we have to use the
+                // transaction object (acTrans) and pass it an ObjectId.
+                var model = (BlockTableRecord)acTrans.GetObject(
+                    acDoc.Database.CurrentSpaceId, OpenMode.ForRead);
+
+                // We will put our polygons here
+                var polygons = new List<AcadPolygon>();
+                // Now we iterate through all objects in the model space.
+                // Notice that we get ObjectIds and not the actual objects.
+                foreach(ObjectId objId in model)
+                {
+                    // Get the actual object
+                    var obj = acTrans.GetObject(objId, OpenMode.ForRead);
+                    // Ignore this object if it is not a Polyline
+                    var polyline = obj as Polyline;
+                    if (polyline == null)
+                        continue;
+
+                    // We will save the points in this AcadPolygon object
+                    var poly = new AcadPolygon();
+                    // Here we iterate through the number of vertices
+                    for (var i = 0; i < polyline.NumberOfVertices; ++i)
+                    {
+                        // Get the i-th vertex of this polyline.
+                        var pt = polyline.GetPoint2dAt(i);
+                        // Create a point suitable to our data structure
+                        var point = new AcadPolygon.Point() {
+                            X = pt.X,
+                            Y = pt.Y
+                        };
+                        // And add it to our AcadPolygon container.
+                        poly.Points.Add(point);
+                    }
+                    // Add this polygon to the collection
+                    polygons.Add(poly);
+                }
+                // Convert the list of polygons to json
+                var json = JsonConvert.SerializeObject(
+                    polygons,
+                    Formatting.Indented);
+                // Create a temporary text file
+                var filename = System.IO.Path.GetTempFileName() + ".txt";
+                // Write this json representation to that file
+                System.IO.File.WriteAllText(filename, json);
+                // Open it with the default text editor
+                System.Diagnostics.Process.Start(filename);
+            }
+        }
+    }
+}
---
+++ Integration-CoreLib/AcadPolygon.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PolygonData
+{
+
+    /// <summary>
+    /// Simple 2D Polygon class.
+    /// This class must be public so that we can use it from other projects.
+    /// </summary>
+    public class AcadPolygon
+    {
+        /// <summary>
+        /// A basic 2D Point
+        /// </summary>
+        public class Point
+        {
+            public double X = 0.0;
+            public double Y = 0.0;
+        }
+        /// <summary>
+        /// This name will be used when we can't find a better one.
+        /// </summary>
+        private static string defaultName = "Polygon";
+        /// <summary>
+        /// Dictionary converting number of vertices to polygon name.
+        /// </summary>
+        private static Dictionary<int, string> polygonNames = new Dictionary<int, string>()
+        {
+            {3, "Triangle" },
+            {4, "Rectangle" },
+            {5, "Pentagon" },
+            {6, "Hexagon" },
+            {7, "Heptagon" },
+            {8, "Octagon" },
+            {9, "Nonagon" },
+            {10, "Decagon" },
+            {11, "Hendecagon" },
+            {12, "Dodecagon" },
+        };
+        /// <summary>
+        /// Return the name of a polygon with a given amount of vertices.
+        /// </summary>
+        /// <param name="vertices">Amount of vertices</param>
+        /// <returns>Name of the polygon</returns>
+        private static string PolygonName(int vertices)
+        {
+            // Check the dictionary and return that value
+            if (polygonNames.ContainsKey(vertices))
+                return polygonNames[vertices];
+            // Otherwise return the default value
+            return defaultName;
+        }
+
+        /// <summary>
+        /// Container for the vertices
+        /// </summary>
+        public List<Point> Points = new List<Point>();
+        public string Name
+        {
+            get
+            {
+                return PolygonName(Points.Count);
+            }
+        }
+    }
+}
